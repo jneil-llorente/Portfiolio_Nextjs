dc015373ef316f669ed500f41fea76f74b8a537b
jneil-llorente <joseneil27@gmail.com>
2025-03-23 18:37:52 +0800

    design - hero page

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -4,10 +4,10 @@ import { PropsWithChildren } from 'react';
 export const HeroOrbit = ({ children, size, rotation }: PropsWithChildren<{ size: number; rotation: number;}>) => {
   return (
   <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <div className="size-[800px]" style={{
+    <div className="flex items-start justify-start" style={{
         transform: `rotate(${rotation}deg)`,
         height: `${size}px`,
-        width: `${size}`
+        width: `${size}px`,
     }}>
       <div className="inline-flex"
       style={{
